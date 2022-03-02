f75385f127b622bcd439bb8c7f4ecb8f73b67a61
BogdanChyornyy <booggiman@mail.ru>
2022-03-02 22:15:05 +0300

    Выполнил домашнее задание по уроку 2, проект №4.

--- Lesson_2.1/Program.cs
+++ Lesson_2.1/Program.cs
@@ -11,20 +11,54 @@ namespace lesson_1_enter
         static void Main(string[] args)
         {
 
-            //3.Проверка числа на четность.
+            //4.Создание чека.
 
-            Console.WriteLine("Введите число для проверки на четность: ");
-            int number = int.Parse(Console.ReadLine());
-            int divis = number % 2;
-            if (divis == 0)
-            {
-                Console.WriteLine("Число " + number + " - четное.");
-            }
-            else
-            {
-                Console.WriteLine("Число " + number + " - не четное.");
-            }
+            string name_of_owner = "ИП Чёрный Б.К.";
+            string defolt = "Кассовый чек";
+            string IE = "ИП ЧЁРНЫЙ БОГДАН КОНСТАНТИНОВИЧ";
+            string cashier = "Иванов И.И.";
+            string location = "Калининградская обл. г. Калининград ул. Нарвская д. 12.";
+            string place = "Кофейня";
 
+            Console.WriteLine("Приветствуем вас в нашей кофейне!");
+            Console.WriteLine("Укажите номер напитка: ");
+            Console.WriteLine("1. Капучино 100р");
+            Console.WriteLine("2. Латте 100р");
+            Console.WriteLine("3. Эспрессо 80р");
+            string[] choice = { "Капучино", "Латте", "Эспрессо" };
+
+            int coffee_number = int.Parse(Console.ReadLine());
+            int real_number = coffee_number -= 1;
+
+            int[] coffee_price = { 100, 100, 80 };
+            int coffee_price_numder = real_number;
+
+            Console.WriteLine("Сколько чашек кофе вы желаете?");
+            int quontity_coffee = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сколько пакетиков сахара вам положить?");
+            Console.WriteLine("Один пакетик сахара 5р.");
+            int sugar = int.Parse(Console.ReadLine());
+            int sugar_price = 5;
+
+            int type = real_number;
+            int total = sugar * sugar_price + quontity_coffee * coffee_price[coffee_price_numder];
+
+            Console.WriteLine(name_of_owner);
+            Console.WriteLine(defolt);
+            Console.WriteLine(choice[type] + " *" + quontity_coffee + " =" + quontity_coffee * coffee_price[coffee_price_numder]);
+            Console.WriteLine("НДС не облагается");
+            Console.WriteLine("Сахар " + " *" + sugar + " =" + sugar * sugar_price);
+            Console.WriteLine("НДС не облагается");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("ИТОГ " + " =" + total);
+            Console.WriteLine("------------------------");
+            Console.WriteLine("СУММА БЕЗ НДС " + " =" + total);
+            Console.WriteLine("Кассир " + cashier);
+            Console.WriteLine(IE);
+            Console.WriteLine(location);
+            Console.WriteLine("Место расчетов " + place);
+            Console.WriteLine("СПАСИБО ЗА ПОКУПКУ!");
         }
     }
 }
