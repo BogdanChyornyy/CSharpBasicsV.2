d0704d00495e11fb464ae3d0f3aa7a464526656b
BogdanChyornyy <booggiman@mail.ru>
2022-03-06 15:22:59 +0300

    Выполнил домашнее задание по уроку 3, проект №3.

--- Lesson_2.1/Program.cs
+++ Lesson_2.1/Program.cs
@@ -10,29 +10,14 @@ namespace lesson_1_enter
     {
         static void Main(string[] args)
         {
-            string[,] directory = new string[5, 2];
-            string[] name = { "John", "Sarah", "Tony", "Nick", "Taylor"};
-            string[] number = { "89111111111", "89222222222", "89333333333", "89444444444", "89555555555"};
+            Console.WriteLine("Введите текст: ");
+            string greeting = Console.ReadLine();
             
-            for (int i = 0; i < 5; i++)
+            for (int i = greeting.Length - 1; i >= 0; i--)
             {
-                for (int j = 0; j < 2; j++) 
-                {
-                    if (j == 0) 
-                    {
-                    directory[i, j] = name[i];
-                    System.Console.Write($"{directory[i, j]} ");
-                    }
-                    else
-                    {
-                    directory[i, j] = number[i];
-                    System.Console.Write($"{directory[i, j]} ");
-                    }
-                    
-                }
-                System.Console.WriteLine();
+                Console.Write(greeting[i]);
             }
-
         }
     }
 }
+
